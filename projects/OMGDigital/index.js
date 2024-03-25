28e6103ea2be0f276e3c74ed0377ad011990e9a8
Mohamad-Gh <146002135+Mohamad-Gh@users.noreply.github.com>
2024-03-26 01:19:56 +0330

    Add files via upload

--- projects/OMGDigital/index.js
+++ projects/OMGDigital/index.js
@@ -1,13 +1,13 @@
 $(".fa-bars").click(function () {
-  $("nav").toggleClass("show");
+  $("nav").toggleClass("show-bar");
   // console.log(this);
 });
 $(document).click(function (event) {
-  if (event.target.id != "menu") {
+  if (event.target.id != "menu-bar") {
     console.log(event.target.id);
-    $("nav").removeClass("show");
+    $("nav").removeClass("show-bar");
   }
 });
 $(document).on("scroll", function (event) {
-  $("nav").removeClass("show");
+  $("nav").removeClass("show-bar");
 });
