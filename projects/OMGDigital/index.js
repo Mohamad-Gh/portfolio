d578b0c5c40a0241c99bfb4c739a1707af027660
Mohamad-Gh <146002135+Mohamad-Gh@users.noreply.github.com>
2024-03-26 05:52:26 +0330

    Add files via upload

--- projects/OMGDigital/index.js
+++ projects/OMGDigital/index.js
@@ -10,4 +10,13 @@ $(document).click(function (event) {
 });
 $(document).on("scroll", function (event) {
   $("nav").removeClass("show-bar");
+  // console.log(this);
+  // console.log(event);
+  console.log($(event.target).scrollTop());
+  // scrollTop shows the position of scroll in numbers
+  if ($(this).scrollTop() >= 3100) {
+    $("#bar-90").addClass("animation-90");
+    $("#bar-96").addClass("animation-96");
+    $("#bar-98").addClass("animation-98");
+  }
 });
