42eda6c2a5c01e7ba318a52cc3a069e12270ee6e
Mohamad-Gh <146002135+Mohamad-Gh@users.noreply.github.com>
2024-04-23 06:23:33 +0330

    Add files via upload

--- projects/OMGDigital/index.js
+++ projects/OMGDigital/index.js
@@ -1,3 +1,24 @@
 $(".fa-bars").click(function () {
-  $("nav").slideToggle();
+  $("nav").toggleClass("show-bar");
+  // console.log(this);
+});
+//closing menu bar with clicking on the page
+$(document).click(function (event) {
+  if (event.target.id != "menu-bar") {
+    // console.log(event.target.id);
+    $("nav").removeClass("show-bar");
+  }
+});
+// close the menu bar with using scroll
+$(document).on("scroll", function (event) {
+  $("nav").removeClass("show-bar");
+
+  // console.log($(event.target).scrollTop());
+  // scrollTop shows the position of scroll in numbers
+  // run the filling animation with scroll
+  if ($(this).scrollTop() >= 3100) {
+    $("#bar-90").addClass("animation-90");
+    $("#bar-96").addClass("animation-96");
+    $("#bar-98").addClass("animation-98");
+  }
 });
